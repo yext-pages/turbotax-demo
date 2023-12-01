98da90b330dd9a884cd91d9a2767341b1464cf01
ipentland <Ian_Pentland@intuit.com>
2023-12-01 11:57:20 -0800

    feat: index template

---
+++ src/templates/indexTemplate.tsx
@@ -0,0 +1,36 @@
+import type {
+  TemplateProps,
+  TemplateRenderProps,
+  GetHeadConfig,
+  GetPath,
+  Template,
+  TemplateConfig,
+} from "@yext/pages";
+
+export const config: TemplateConfig = {
+  // The name of the feature. If not set the name of this file will be used (without extension).
+  // Use this when you need to override the feature name.
+  name: "index",
+};
+
+// The path must be exactly 404.html
+export const getPath: GetPath<TemplateProps> = () => {
+  return "index.html";
+};
+
+// Add a title to the page
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = () => {
+  return {
+    title: "TurboTax® Verified Pro",
+  };
+};
+
+const IndexTemplate: Template<TemplateRenderProps> = () => {
+  return (
+    <div>
+      <h1>Index Template</h1>
+    </div>
+  );
+}
+
+export default IndexTemplate;

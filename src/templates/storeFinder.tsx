785d68ea6e7f6034c8fb6207effd1c35cb896b02
ipentland <Ian_Pentland@intuit.com>
2023-12-13 16:12:53 -0800

    feat: [FTPP-168] pro directory

---
+++ src/templates/storeFinder.tsx
@@ -0,0 +1,33 @@
+import { GetPath, TemplateConfig, TemplateProps, Template } from "@yext/pages";
+import { GetHeadConfig, TemplateRenderProps } from "@yext/pages/dist/types/src";
+import matchingPreviewHeader from "../assets/content/matchingPreviewHeader";
+import "../index.css";
+
+export const config: TemplateConfig = {
+  name: "Store Finder",
+};
+
+export const getPath: GetPath<TemplateProps> = () => {
+  return "store-finder";
+};
+
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = () => {
+  return {
+    title: "TurboTax® Verified Pro | Pro Directory",
+    tags: matchingPreviewHeader,
+  };
+};
+
+const StoreFinder: Template<TemplateRenderProps> = () => {
+  return (
+    <iframe
+      src={"https://search-embed.pros.turbotax.intuit.com.pagescdn.com"}
+      title={"TurboTax Verified Pro Directory"}
+      // Please ignore your IDE's warning about the two classes doing the same thing.
+      // Not all browsers support dvh, so we include the first as a fallback.
+      className={"w-full h-screen h-[100dvh] border-none"}
+    />
+  );
+};
+
+export default StoreFinder;

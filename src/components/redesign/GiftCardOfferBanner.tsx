e83615ed149c2c8936f59fab7e006e8c1864d830
Tsipporah Christopher <tsipporah_christopher@intuit.com>
2024-03-05 20:28:05 -0800

    feat(FTDC-1239): add pro photo to banner

--- src/components/redesign/GiftCardOfferBanner.tsx
+++ src/components/redesign/GiftCardOfferBanner.tsx
@@ -2,20 +2,31 @@ import React, { FC } from 'react';
 import Link from "../atoms/Link";
 import { TypeScale } from "../atoms/TypeScale";
 import ResponsiveTypography from "../atoms/ResponsiveTypography";
+import useIndependentPro from "../../hooks/useIndependentPro";
 
 const GiftCardOfferBannerWrapper = 'bg-tofu120';
 const GiftCardOfferBannerWrapper2 = 'flex justify-center items-center flex-row gap-4 px-2';
-const GiftCardOfferBannerImg = 'max-w-full hidden md:block';
+const GiftCardOfferBannerImg = 'max-w-full hidden min-[375px]:block';
 const GiftCardOfferBannerContent = 'text-gray-700 flex flex-col justify-center items-start';
 const GiftCardOfferBannerTitle = 'text-2xl font-extrabold leading-7 md:text-6xl md:leading-14';
 const GiftCardOfferBannerSubTitleWrapper = 'flex justify-center items-center flex-row gap-2';
 
 
 const GiftCardOfferBanner: FC = () => {
+  const pro = useIndependentPro();
+
   return (
     <div className={GiftCardOfferBannerWrapper}>
       <div className={GiftCardOfferBannerWrapper2}>
-        <img className={GiftCardOfferBannerImg} srcSet={"#"} alt="" />
+        <div className={GiftCardOfferBannerImg}>
+          <img
+               src={pro.c_epsImageUrl || pro.headshot?.url}
+               alt={pro.c_taxProName}
+               height={32}
+               width={"auto"}
+               className={"border-solid border-pepper100 border-1 rounded-full object-contain max-h-[32px] max-w-[32px] l:max-h-[64px] l:max-w-[64px]"}
+          />
+        </div>
         <div className={GiftCardOfferBannerContent}>
           <div className={GiftCardOfferBannerTitle}>
             <ResponsiveTypography

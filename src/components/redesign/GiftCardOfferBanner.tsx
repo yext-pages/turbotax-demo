457906c418acd73f9ec08d98a78556a5955bf332
Tsipporah Christopher <tsipporah_christopher@intuit.com>
2024-03-06 11:50:36 -0800

    feat(FTDC-1239): clean up code

--- src/components/redesign/GiftCardOfferBanner.tsx
+++ src/components/redesign/GiftCardOfferBanner.tsx
@@ -4,21 +4,13 @@ import { TypeScale } from "../atoms/TypeScale";
 import ResponsiveTypography from "../atoms/ResponsiveTypography";
 import useIndependentPro from "../../hooks/useIndependentPro";
 
-const GiftCardOfferBannerWrapper = 'bg-tofu120';
-const GiftCardOfferBannerWrapper2 = 'flex justify-center items-center flex-row gap-4 px-2';
-const GiftCardOfferBannerImg = 'w-16 hidden min-[375px]:block py-2';
-const GiftCardOfferBannerContent = 'text-gray-700 flex flex-col justify-center items-start';
-const GiftCardOfferBannerTitle = 'text-2xl font-extrabold leading-7 md:text-6xl md:leading-14';
-const GiftCardOfferBannerSubTitleWrapper = 'flex justify-center items-center flex-row gap-2';
-
-
 const GiftCardOfferBanner: FC = () => {
   const pro = useIndependentPro();
 
   return (
-    <div className={GiftCardOfferBannerWrapper}>
-      <div className={GiftCardOfferBannerWrapper2}>
-        <div className={GiftCardOfferBannerImg}>
+    <div className={"bg-tofu120"}>
+      <div className={"flex justify-center items-center flex-row gap-4 px-2"}>
+        <div className={"w-16 hidden min-[375px]:block py-2"}>
           <img
                src={pro.c_epsImageUrl || pro.headshot?.url}
                alt={pro.c_taxProName}
@@ -26,8 +18,8 @@ const GiftCardOfferBanner: FC = () => {
                className={"border-solid border-pepper100 border-1 rounded-full object-contain max-w-full max-h-full"}
           />
         </div>
-        <div className={GiftCardOfferBannerContent}>
-          <div className={GiftCardOfferBannerTitle}>
+        <div className={"text-gray-700 flex flex-col justify-center items-start"}>
+          <div className={"text-2xl font-extrabold leading-7 md:text-6xl md:leading-14"}>
             <ResponsiveTypography
               typescaleMobile={TypeScale.Headline04}
               typescaleStationary={TypeScale.Headline02}
@@ -39,7 +31,7 @@ const GiftCardOfferBanner: FC = () => {
             </ResponsiveTypography>
           </div>
 
-          <div className={GiftCardOfferBannerSubTitleWrapper}>
+          <div className={"flex justify-center items-center flex-row gap-2"}>
               <ResponsiveTypography
                 typescaleMobile={TypeScale.Body04}
                 typescaleStationary={TypeScale.Body01}
